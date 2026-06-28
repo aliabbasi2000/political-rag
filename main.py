e558500d26e85af502ed56b0fe2a2a3c89f002d5
Ali Abbasi <ali.abbasi797979@gmail.com>
2026-06-28 19:15:46 +0200

    Refactor input to get the prompt in a loop

--- main.py
+++ main.py
@@ -1,26 +1,33 @@
 import os
 from ollama import Client
-import sys
 
 from src.prepare_content import search_by_query
 
-query = "Tell me about children's rights in Iran."
-if len(sys.argv) > 1:
-    query = sys.argv[1]
-context = search_by_query(query)
+query = input("Ask me about Politics (or press enter to tell you about the Politics in Italy): ")
+if not query or query.strip() == "":
+    query = "Tell me about Politics in Italy."
 
-prompt = f"<|content> {context}<|content_end> {query}"
-print(f"\nPrompt:\n{prompt}\n")
+while True:
+  context = search_by_query(query)
+
+  prompt = f"<|content_start> {context}<|content_end> {query}"
+  #print(f"\nPrompt:\n{prompt}\n")
 
 
-host = os.environ.get("OLLAMA_HOST", "http://localhost:11434")
-client = Client(host=host)
+  host = os.environ.get("OLLAMA_HOST", "http://localhost:11434")
+  client = Client(host=host)
 
-response = client.chat(model='qwen3:0.6b', messages=[
-  {
-    'role': 'user',
-    'content': prompt,
-  },
-])
+  response = client.chat(model='custom_qwen', messages=[
+    {
+      'role': 'user',
+      'content': prompt,
+    },
+  ])
 
-print(response.message.content)
+  print(f"\n{response.message.content}\n")
+
+  new_query = input("\n Chat (or 'q' to quit): ")
+  if new_query.lower() != 'q':
+    query = new_query
+  else:
+     break
